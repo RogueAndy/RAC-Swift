3181746b94496d5952df1813450fcc5abe8a4b72
Rogue Andy <zlj_1969_love@126.com>
2017-03-20 13:39:13 +0800

    导入依赖项目

--- RAC-Swift/RAC-Swift/AppDelegate.swift
+++ RAC-Swift/RAC-Swift/AppDelegate.swift
@@ -13,9 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window! = UIWindow.init(frame: UIScreen.main.bounds)
+        let one = RoneViewController()
+        let vc = UINavigationController.init(rootViewController: one)
+        self.window!.rootViewController = vc
+        self.window!.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ RAC-Swift/RAC-Swift/ViewControllers/RoneViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RoneViewController.swift
+//  RAC-Swift
+//
+//  Created by rogue on 2017/3/20.
+//  Copyright © 2017年 RogueAndy. All rights reserved.
+//
+
+import UIKit
+
+class RoneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .orange
+    }
+
+}
